13628f271d3cfd4fab3f6230459e36fd7ae38d25
unknown <rafikakatsuki3@gmail.com>
2023-08-14 21:20:18 +0700

    ubah

--- resources/views/detailTouristAttraction.blade.php
+++ resources/views/detailTouristAttraction.blade.php
@@ -37,7 +37,7 @@
             </div>
 
             <div class="gambar-wisata mt-3 w-100" style=" display:flex;align-items:center;justify-content: center;">
-                <img src='{{ asset('storage/'.$tourist_attractions -> image_post) }}' alt="{{$tourist_attractions -> name}}" class = " rounded" style="height:400px; width:850px;">
+                <img src='{{ asset('storage/'.$tourist_attractions -> image_post) }}' alt="{{$tourist_attractions -> name}}" class = " rounded" style="height:500px; width:900px;">
             </div>
 
             <div class="informasi-singkat mt-5">
