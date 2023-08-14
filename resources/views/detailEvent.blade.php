72c570752853e2447422bff8075dc8ac27b4aa14
unknown <rafikakatsuki3@gmail.com>
2023-08-14 21:06:52 +0700

    ubah

--- resources/views/detailEvent.blade.php
+++ resources/views/detailEvent.blade.php
@@ -24,7 +24,7 @@
                 </div>
             </div>
             
-            <div class="gambar-wisata mt-3 w-100" style="height: 500px; display:flex;align-items:center;justify-content: center;">
+            <div class="gambar-wisata mt-3  w-90" style="height: 500px; display:flex;align-items:center;justify-content: center;">
                 <img src='{{ asset('storage/'.$events -> image_post) }}' alt="{{$events -> name}}" class = "h-100 rounded">
             </div>
 
